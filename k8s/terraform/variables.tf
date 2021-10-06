d8cfc790e623089260b0254be0746f6a3023e4be
Rohit Misra <2770353+rohitmisra@users.noreply.github.com>
2021-10-06 14:04:38 +0000

    declare vars

--- k8s/terraform/variables.tf
+++ k8s/terraform/variables.tf
@@ -1,3 +1,17 @@
 variable "ssh_key_location" {
   default = "~/.ssh/id_rsa"
 }
+variable "pm_api_url" {
+  default = ""
+}
+
+variable "master_ip" {
+    description = "IP of the master node"
+	  default     = ""
+}
+
+variable "worker_ips" {
+    description = "IPs of the worker nodes"
+    type        = list(string)
+	  default     = []
+}
